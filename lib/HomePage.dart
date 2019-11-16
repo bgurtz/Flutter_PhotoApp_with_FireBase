c63f07c5551e29cd4e757a7d12c01df4241e6fd6
bgurtz <bgurtz@gmail.com>
2019-11-16 01:05:46 -0600

    " Added Photos to FireBase and set up the UI views "

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -8,6 +10,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+//* The Stream Subscription below is what we are using to connect 
+//* to FireBase to call our information from the DataBase and display it.
+//* This Subscription is for the Horizontal Scroll at the top of the app.
+  StreamSubscription<QuerySnapshot>subscription;
+  List<DocumentSnapshot>snapshot;
+
+  CollectionReference collectionReference=Firestore.instance.collection("TopPost");
+
+  //* This Subscription for FireBase connect is for the ' Body '
+  //* This will be the Vertical Scroll 
+  StreamSubscription<QuerySnapshot>sdSubscription;
+  List<DocumentSnapshot>sdSnapshot;
+
+  CollectionReference sdcollectionReference=Firestore.instance.collection("BodyPost");
+
+  @override
+  void initState() {
+  //* TopPost
+    subscription=collectionReference.snapshots().listen((datasnapshot) {
+      setState(() {
+        snapshot=datasnapshot.documents;
+          }
+        );
+      }
+    );
+//* BodyPost
+    sdSubscription=sdcollectionReference.snapshots().listen((sddatasnap) {
+      sdSnapshot=sddatasnap.documents;
+      }
+    );
+
+    super.initState();
+  }
+  // End of the FireBase connect code for the TopPost
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,6 +117,123 @@ class _HomeState extends State<Home> {
 
           ],
         ),
+      ),
+
+
+//* The code below is the Body of our app
+//* This is what we are going to dispaly and
+//* This is were the information from FireBase will be called to.
+//* We have made a Container with a Card to dispaly our photos and 
+//* gave it a Scroll Direction of Horizontal
+//* our ClipRRect is being used to call out picture from FireBase
+
+      body:  ListView(
+        children: <Widget>[
+
+//* TopPost Container
+          Container(
+            height: 200,
+            child: ListView.builder(
+              itemCount: snapshot.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Card(
+                  // color: Colors.brown[100],
+                  elevation: 10.0,
+                  margin: EdgeInsets.all(5.0),
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    child: Column(
+                    children: <Widget>[
+                     
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(snapshot[index].data["url"],
+                        height: 130.0,
+                          width: 130.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+
+                      SizedBox(height: 10.0,),
+                      Text(snapshot[index].data["title"],
+                        style: TextStyle(fontSize: 18.0, color: Colors.blue[600]),
+                      ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+//* End of the TopPost Container
+
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: ListView.builder(
+              // scrollDirection: Axis.vertical,
+              itemCount: sdSnapshot.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  // color: Colors.brown[100],
+                  elevation: 7.0,
+                  margin: EdgeInsets.all(10.0),
+                  child: Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[
+
+                        Row(
+                          children: <Widget>[
+
+                            CircleAvatar(
+                              child: Text(sdSnapshot[index].data["title"][0]),
+                              backgroundColor: Colors.brown[200],
+                              foregroundColor: Colors.white,
+                            ),
+                            SizedBox(width: 10.0,),
+                            Text(sdSnapshot[index].data["title"],
+                              style: TextStyle(fontSize: 20.0, color: Colors.blue[600]),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10.0,),
+                        Column(
+                          children: <Widget>[
+
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15.0),
+                              child: Image.network(sdSnapshot[index].data["url"],
+                              height: 150.0,
+                              width: MediaQuery.of(context).size.width,
+                               fit: BoxFit.cover,
+                              ),
+                            ),
+                            
+
+                            SizedBox(height: 10.0),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Icon(Icons.share),
+                                Icon(Icons.thumb_up),
+                                Icon(Icons.thumb_down),
+                                Icon(Icons.favorite),
+                              ],
+                            ),
+
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+
+        ],
       ),
 
     );
